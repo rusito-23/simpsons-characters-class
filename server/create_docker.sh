9683a46ba71755b9bb2f83c98e40ea010cca5da1
Rusito23 <i.andruskiewitsch23@gmail.com>
2019-12-29 23:25:35 -0300

    - fix app - still not predicting right -

--- server/create_docker.sh
+++ server/create_docker.sh
@@ -5,4 +5,4 @@ docker cp model/lesimpson serving_base:/models/lesimpson
 docker commit --change "ENV MODEL_NAME lesimpson" serving_base lesimpson_serve
 docker kill serving_base
 docker rm serving_base
-docker run --name lesimpson_serve lesimpson_serve
+docker run -p 8501:8501 --name lesimpson_serve lesimpson_serve
--- server/source/app/app.py
+++ server/source/app/app.py
@@ -10,6 +10,40 @@ from PIL import Image
 import cv2
 import requests
 
+LABELS = [
+'maggie_simpson',
+'charles_montgomery_burns',
+'patty_bouvier',
+'ralph_wiggum',
+'chief_wiggum',
+'milhouse_van_houten',
+'martin_prince',
+'lenny_leonard',
+'sideshow_bob',
+'selma_bouvier',
+'barney_gumble',
+'moe_szyslak',
+'carl_carlson',
+'edna_krabappel',
+'snake_jailbird',
+'groundskeeper_willie',
+'ned_flanders',
+'abraham_grampa_simpson',
+'krusty_the_clown',
+'waylon_smithers',
+'apu_nahasapeemapetilon',
+'marge_simpson',
+'comic_book_guy',
+'nelson_muntz',
+'mayor_quimby',
+'kent_brockman',
+'professor_john_frink',
+'principal_skinner',
+'bart_simpson',
+'lisa_simpson',
+'homer_simpson',
+]
+
 # APP
 
 app = Flask(__name__)
@@ -20,17 +54,24 @@ def image_classifier():
     image = request.files["image"]
     image = Image.open(BytesIO(image.read()))
     image = np.array(image)
-    image = cv2.resize(image, (128, 128))
+    image = cv2.resize(image, (50, 50))
 
     payload = {
-        "instances": [{'input': [image.tolist()]}]
+        "instances": [{'in': image.tolist()}]
     }
 
     # Making POST request
     r = requests.post('http://localhost:8501/v1/models/lesimpson:predict', json=payload)
 
     # Decoding results from TensorFlow Serving server
-    pred = json.loads(r.content.decode('utf-8'))
+    pred = json.loads(r.content.decode('utf-8'))['predictions'][0]
+
+    i = 0
+    for p in pred:
+        if p != 0:
+            prediction = LABELS[i]
+            break
+        i += 1
 
     # Returning JSON response to the frontend
-    return jsonify(inception_v3.decode_predictions(np.array(pred['predictions']))[0])
+    return jsonify({"index": i, "pred": prediction})
